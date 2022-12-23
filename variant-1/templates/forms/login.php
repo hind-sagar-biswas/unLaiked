1d2320d6b3d66d6f05ca3cfdb9edd710e6a59bee
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-12-23 22:54:25 +0600

    completely removed var1

--- variant-1/templates/forms/login.php
+++
@@ -1,6 +0,0 @@
-<h3>LOGIN AS: </h3>
-<form action="<?= $INC . 'user.inc.php' ?>" method="post">
-    <input type="number" name="user-id" id="user-id" required>
-    <input type="text" name="username" id="username" required>
-    <button type="submit" name="login" id="login" value="login">lLOGIN</button>
-</form>
